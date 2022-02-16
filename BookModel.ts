22f061624f024ab2586c3fd5c775bec014bfaf37
Chris Merris <chris.merris@gmail.com>
2022-02-16 16:26:59 -0700

    updates and notes

--- BookModel.ts
+++ BookModel.ts
@@ -1,34 +1,65 @@
-import * as Joi from 'joi';
-interface IBookKeys {
-  bookId: string;
-}
+import { Number, Record, Static, String } from 'runtypes';
 
-interface IBookContent {
-  text: string;
-  pages: number;
-}
+/**
+ * Scratchwork trying out runtypes - Book/Author schema
+ * with some contrived runtime constraints in addition to types
+ * Using asReadOnly pervasively to make types immutables
+ */
+export const Author = Record({
+  name: String.withConstraint(
+    (name) => name.length > 0 || 'name must be non-empty'
+  ),
+  age: Number.withConstraint((age) => age < 100 || 'age must be < 100'),
+})
+  .asReadonly()
+  .withConstraint(
+    (author) =>
+      author.name === `${author.age}` || 'name must match age as a string'
+  );
 
-type IBookModel = Readonly<IBookKeys & IBookContent>;
+export const BookKeys = Record({
+  bookId: String,
+}).asReadonly();
 
-export const BookModelSchema = Joi.object({
-  bookId: Joi.string(),
-  text: Joi.string(),
-  pages: Joi.number(),
-});
+export const BookContent = Record({
+  author: Author,
+  text: String.optional(),
+  pages: Number,
+}).asReadonly();
 
-function isBookModel(bookModel: any): bookModel is IBookModel {
-  const validationResult = BookModelSchema.validate(bookModel);
+// Composing/mutating two schemas into one
+export const Book = BookKeys.And(BookContent);
 
-  if (validationResult.error) {
-    throw new Error(validationResult.error.message);
-  } else {
-    return true;
-  }
-}
+// inferred type is intersection of Book and BookContent inferred types
+// equivalent to typescript & operator
+type Book = Static<typeof Book>;
 
 function test() {
-  const book1 = { bookId: 'abc', text: 'xyz', pages: 123 };
-  if (isBookModel(book1)) {
-    book1.bookId;
-  }
+  // Throws error if not valid
+  const book1 = Book.check({
+    bookId: 'abc',
+    text: 'xyz',
+    pages: 123,
+    author: {
+      name: '99',
+      age: 99,
+    },
+  });
+
+  // Type of book1 now known, valid fields accessible
+  book1.bookId;
+  book1.pages;
+  book1.author.name;
+  book1.author.age;
+
+  // Text field is optional
+  book1.text;
+
+  // Compiler error: invalid field
+  book1.junk;
+
+  // Compiler error: all field are readonly (immutable)
+  book1.bookId = 'abc';
 }
+
+test();
---
+++ README.md
@@ -0,0 +1,4 @@
+# runtypes testing
+
+to compile and run (intentional compilation errors currently in code to illustrate type inference in runtypes):
+`npm run book-model-test`
